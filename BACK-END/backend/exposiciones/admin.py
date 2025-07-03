7ef90685571a5b8940b163286443a59ecda61148
Cbas1417 <sj153175@gmail.com>
2025-07-03 09:27:21 -0500

    prueba

--- BACK-END/backend/exposiciones/admin.py
+++ BACK-END/backend/exposiciones/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from exposiciones.models import Exposicion
 
 # Register your models here.
+
+class ExposicionAdmin(admin.ModelAdmin):
+    list_display=("nombre","tiempo")#para escojer que campos quiero ver en el admin
+    search_fields=("nombre",)#crear un registro de busqueda segun estos campos
+
+
+admin.site.register(Exposicion,ExposicionAdmin)
