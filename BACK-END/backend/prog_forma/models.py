65f4a2965b15b2ef16f2364434d6e3ff3355ed60
juan <jdgarcia220406@gmail.com>
2025-08-20 19:17:36 -0500

    mejorar

--- BACK-END/backend/prog_forma/models.py
+++ BACK-END/backend/prog_forma/models.py
@@ -2,23 +2,19 @@ from django.db import models
 from autoslug import AutoSlugField
 from usuarios.models import Usuario
 
+# Create your models here.
+
 class Programa(models.Model):
-    CATEGORIA_CHOICES = [
-        ('ece', 'Artes escénicas'),
-        ('visu', 'Artes visuales'),
-    ]
-    
-    foto_programa = models.ImageField(upload_to='prog_forma/')
-    titulo = models.CharField(max_length=100, null=True)
+    foto_programa=models.ImageField(upload_to='prog_forma/')
+    titulo=models.CharField(max_length=100,null=True)
     slug = AutoSlugField(populate_from='titulo')
-    descripcion = models.TextField(null=True)
-    categoria = models.CharField(max_length=4, choices=CATEGORIA_CHOICES, default='ece')
+    descripcion=models.TextField(null=True)
     usuarios = models.ManyToManyField(Usuario, blank=True, related_name='programas')
     
     def __str__(self):
         return self.titulo
 
-    class Meta:
-        db_table = 'programa'
-        verbose_name = 'Programa'
-        verbose_name_plural = 'Programas'
+    class meta:
+        db_table='programa'
+        verbose_name='Programa'
+        verbose_name_plural='Programas'     
--- BACK-END/backend/prog_forma/urls.py
+++ BACK-END/backend/prog_forma/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from .views import *
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import ProgramasListCreateView, ProgramasUpdateDeleteView, InscripcionPrograma
+
 
 urlpatterns = [
-    path('api/programas/prog_forma/get_post/', class1.as_view()),  # Corregida la URL
-    path('api/programas/prog_forma/put_delete/<int:id>/', class2.as_view()),  # Corregida la URL
-    path('api/programas/inscribirse/<int:id>/', class3.as_view()),  # Mantenida igual
+    path('prog_forma/get_post',class1.as_view()),
+    path('prog_forma/put_delete/<int:id>',class2.as_view()),
+    path('programas/<int:id>/inscribir/', InscripcionPrograma.as_view(), name='programa_inscribir'),
 ]
 
 if settings.DEBUG:
