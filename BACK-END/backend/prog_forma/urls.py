d4ea91a2b53e946a9bbd244d177fdf988969b802
Cbas1417 <sj153175@gmail.com>
2025-08-25 08:56:57 -0500

    Resuelto merge entre remoto y local

--- BACK-END/backend/prog_forma/urls.py
+++ BACK-END/backend/prog_forma/urls.py
@@ -2,19 +2,11 @@ from django.urls import path
 from .views import *
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import ProgramasListCreateView, ProgramasUpdateDeleteView, InscripcionPrograma
 
-
-urlpatterns = [
-<<<<<<< HEAD
-    path('api/programas/prog_forma/get_post/', class1.as_view()),
-    path('api/programas/prog_forma/put_delete/<int:id>/', class2.as_view()),  
-    path('api/programas/inscribirse/<int:id>/', class3.as_view()),  
-=======
-    path('prog_forma/get_post',class1.as_view()),
-    path('prog_forma/put_delete/<int:id>',class2.as_view()),
-    path('programas/<int:id>/inscribir/', InscripcionPrograma.as_view(), name='programa_inscribir'),
->>>>>>> e26655d9db14448c387f6bab434a48cd1c891960
+urlpatterns=[
+    path('prog_forma/get_post', class1.as_view()),
+    path('prog_forma/put_delete/<int:id>', class2.as_view()),
+    path('prog_forma/<int:id>/inscribir', class3.as_view()),
 ]
 
 if settings.DEBUG:
