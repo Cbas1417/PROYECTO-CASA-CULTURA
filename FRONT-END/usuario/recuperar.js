9380834aa8edfbfc62cfbc5151036ef699a716d3
juan <canojuanesesteban23@gmail.com>
2025-09-08 13:30:32 -0500

    recuperar

--- FRONT-END/usuario/recuperar.js
+++ FRONT-END/usuario/recuperar.js
@@ -1,22 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-const form = document.querySelector("form.carta");
-const modal = document.getElementById("modal-success");
+  const form = document.querySelector("form.carta");
+  const modal = document.getElementById("modal-success");
 
-form.addEventListener("submit", function (e) {
+  form.addEventListener("submit", async function (e) {
     e.preventDefault(); 
     const emailInput = document.getElementById("email");
-    const email = emailInput.value.trim();
+    const correo = emailInput.value.trim();
 
-    if (email === "" || !email.includes("@")) {
-    alert("Por favor, ingresa un correo válido.");
-    return;
+    if (correo === "" || !correo.includes("@")) {
+      alert("Por favor, ingresa un correo válido.");
+      return;
     }
 
-    modal.style.display = "flex";
+    try {
+      const res = await axios.post("http://localhost:8000/api/v1/seguridad/recuperar", { correo });
 
-    setTimeout(() => {
-    window.location.href = "iniciar.html";
-    }, 2000);
-});
-});
+      console.log("Respuesta del backend:", res.data);
 
+      modal.style.display = "flex";
+
+      setTimeout(() => {
+        window.location.href = "iniciar.html";
+      }, 2000);
+
+    } catch (err) {
+      console.error("Error:", err);
+      if (err.response) {
+        alert(err.response.data.mensaje || "Error al enviar el correo");
+      } else {
+        alert("Error de red. Intenta nuevamente.");
+      }
+    }
+  });
+});
