a4f684fcb855bf0e1c1eb2316184ccbcb648f091
juan <jdgarcia220406@gmail.com>
2025-09-27 15:52:51 -0500

    mejorar

--- BACK-END/backend/prog_forma/urls.py
+++ BACK-END/backend/prog_forma/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls.static import static
 urlpatterns=[
     path('prog_forma/get_post', class1.as_view()),
     path('prog_forma/put_delete/<int:id>', class2.as_view()),
-    path('prog_forma/<int:id>/inscribir', class3.as_view()),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
